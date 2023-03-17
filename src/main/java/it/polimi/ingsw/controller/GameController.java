04b7a9f0a85f5bdc26d7c4b68e7923611a5f5295
Amuro <federico.zanca@mail.polimi.it>
2023-03-17 23:08:25 +0100

    Minor change for git testing

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -1,8 +1,11 @@
 package it.polimi.ingsw.controller;
+import it.polimi.ingsw.model.Bag;
 import it.polimi.ingsw.model.Player;
 
 import java.util.ArrayList;
 
 public class GameController {
     private ArrayList<Player> players;
+    private Bag bag;
+
 }
