0a1bf3fc8f2d2092c7f61ec0bf4f831c8372ab8a
ZGMIKE2000 <zhenghaomichele.zhuge@mail.polimi.it>
2023-03-19 18:11:39 +0100

    Added CommonGoalCards, added attribute GroupId in ItemTile

--- src/main/java/it/polimi/ingsw/model/ItemTile.java
+++ src/main/java/it/polimi/ingsw/model/ItemTile.java
@@ -5,12 +5,17 @@ import it.polimi.ingsw.model.enumerations.ItemType;
 public class ItemTile {
     private ItemType type;
 
+    private int GroupId;
+
     public ItemTile(ItemType type){
         this.type = type;
     }
     public void setType(ItemType type) {
         this.type = type;
     }
+
+    public void setGroupId(int GroupId){ this.GroupId = GroupId; }
+    public int getGroupId(){ return GroupId; }
 
     public ItemType getType() {
         return type;
