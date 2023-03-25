e682a14c6cc85795b2a9726684c5e0027f359ab7
Amuro <federico.zanca@mail.polimi.it>
2023-03-25 12:20:30 +0100

    Added endgame handler that calculates points to assign to each player

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -2,10 +2,14 @@ package it.polimi.ingsw.controller;
 
 
 import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.enumerations.GamePhase;
 
-public class GameController {
+import java.util.ArrayList;
 
+public class GameController {
+    private static final int MIN_PLAYERS = 2;
+    private static final int MAX_PLAYERS = 3;
     private Game game;
     private GameLobby lobby;
     private GamePhase gamePhase;
@@ -34,7 +38,38 @@ public class GameController {
         return game;
     }
 
+    /** Method called to calculate points to add to each player and declare the winner
+     */
+    public void endGame(){
+        Player winner;
+        assignPersonalGoalPoints();
+        assignAdjacentItemsPoints();
+        //TODO fai funzione scelta vincitore
+        //winner = declareWinner();
 
+    }
+
+    private void assignAdjacentItemsPoints() {
+        int points;
+        ArrayList<Player> players = game.getPlayers();
+        for(Player p : players){
+            points = p.calculateAdjacentItemsPoints();
+            p.addPoints(points);
+        }
+    }
+
+
+    /**
+     * Add personal goal points for each player
+     */
+    public void assignPersonalGoalPoints(){
+        int points;
+        ArrayList<Player> players = game.getPlayers();
+        for(Player p : players){
+            points = p.calculateScorePersonalGoal();
+            p.addPoints(points);
+        }
+    }
     //public
 
     //game initialization : preparing board, extracting common goals...
