90f7d24e7377a8a10adb9012fa552bf270ff47c3
Jhui0908 <jiahui.wu@mail.polimi.it>
2023-03-25 17:43:51 +0100

    fixed check method of CommonGoalCard4 class

--- src/main/java/it/polimi/ingsw/model/commongoals/CommonGoalCard4.java
+++ src/main/java/it/polimi/ingsw/model/commongoals/CommonGoalCard4.java
@@ -17,42 +17,33 @@ public class CommonGoalCard4 extends CommonGoalCard{
         // controllo le righe e le colonne tranne ultima colonna e riga
         for (int i = 0; i < NROW-1; i++) {
             for (int j = 0; j < NCOL-1; j++) {
-                ItemTile topLeft1 = matrix[i][j];
-                ItemTile topRight1 = matrix[i][j+1];
-                ItemTile downLeft1 = matrix[i+1][j];
-                ItemTile downRight1 = matrix[i+1][j+1];
-                if (topLeft1.getType() != ItemType.EMPTY
-                        && topLeft1.getType() == topRight1.getType()
-                        && topRight1.getType() == downLeft1.getType()
-                        && downLeft1.getType() == downRight1.getType()) {
-                    matrix[i][j].setGroupId(1);
-                    matrix[i][j+1].setGroupId(1);
-                    matrix[i+1][j].setGroupId(1);
-                    matrix[i+1][j+1].setGroupId(1);
-                    // una volta trovato prima squadra 2x2 controllo per altre celle
+                if (matrix[i][j].getType() != ItemType.EMPTY
+                        && matrix[i][j].getType() == matrix[i][j+1].getType()
+                        && matrix[i][j+1].getType() == matrix[i+1][j].getType()
+                        && matrix[i+1][j].getType() == matrix[i+1][j+1].getType()
+                        && checkAdjDifferentType (matrix, i, j, "topLeft")
+                        && checkAdjDifferentType (matrix, i, j+1, "topRight")
+                        && checkAdjDifferentType (matrix, i+1, j, "downLeft")
+                        && checkAdjDifferentType (matrix, i+1, j+1, "downRight")) {
+
+                    // una volta trovato prima squadra 2x2 controllo per le celle rimanenti
                     for (int h = i; h < NROW-1; h++) {
-                        if (h == i){
-                            // se sono sulla stessa riga controllo da 2 collone succesive
+                        if (h == i || h == i+1){
+                            // se sono sulla stesse righe controllo da 2 collone succesive
                             k_init = j + 2;
                         }else{
                             // se non sono sulla stessa riga controllo da prima colonna
                             k_init = 0;
                         }
                         for (int k = k_init; k < NCOL-1; k++) {
-                            ItemTile topLeft2 = matrix[h][k];
-                            ItemTile topRight2 = matrix[h][k+1];
-                            ItemTile downLeft2 = matrix[h+1][k];
-                            ItemTile downRight2 = matrix[h+1][k+1];
-                            if (downLeft2.getGroupId() != 0 ||
-                                    topLeft2.getGroupId() != 0 ||
-                                    downRight2.getGroupId() != 0 ||
-                                    topRight2.getGroupId() != 0){
-                                continue;
-                            }
-                            if (topLeft2.getType() != ItemType.EMPTY
-                                    && topLeft2.getType() == topRight2.getType()
-                                    && topRight2.getType() == downLeft2.getType()
-                                    && downLeft2.getType() == downRight2.getType()) {
+                            if (matrix[h][k].getType() != ItemType.EMPTY
+                                    && matrix[h][k].getType() == matrix[h][k+1].getType()
+                                    && matrix[h][k+1].getType() == matrix[h+1][k].getType()
+                                    && matrix[h+1][k].getType() == matrix[h+1][k+1].getType()
+                                    && checkAdjDifferentType (matrix, h, k, "topLeft")
+                                    && checkAdjDifferentType (matrix, h, k+1, "topRight")
+                                    && checkAdjDifferentType (matrix, h+1, k, "downLeft")
+                                    && checkAdjDifferentType (matrix, h+1, k+1, "downRight")) {
                                 return true;
                             }
                         }
@@ -63,4 +54,31 @@ public class CommonGoalCard4 extends CommonGoalCard{
         return false;
     }
 
+    public boolean checkAdjDifferentType (ItemTile[][] matrix, int i, int j, String position){
+        if (position.equals("topLeft")){
+            if (( i == 0 || matrix[i-1][j].getType() !=  matrix[i][j].getType())
+                    && (j == 0 || matrix[i][j-1].getType() !=  matrix[i][j].getType())){
+                return true;
+            }
+        }else if (position.equals("topRight")){
+            if (( i == 0 || matrix[i-1][j].getType() !=  matrix[i][j].getType())
+                    && (j == NCOL-1 || matrix[i][j+1].getType() !=  matrix[i][j].getType())){
+                return true;
+            }
+        }else if (position.equals("downLeft")){
+            if (( i == NROW-1 || matrix[i+1][j].getType() !=  matrix[i][j].getType())
+                    && (j == 0 || matrix[i][j-1].getType() !=  matrix[i][j].getType())){
+                return true;
+            }
+        }else{
+            if (( i == NROW-1 || matrix[i+1][j].getType() !=  matrix[i][j].getType())
+                    && (j == NCOL-1 || matrix[i][j+1].getType() !=  matrix[i][j].getType())){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
+
+
