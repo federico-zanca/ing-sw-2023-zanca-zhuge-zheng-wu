b5172ab5f109583bc590906094cebc954df1296b
ZGMIKE2000 <zhenghaomichele.zhuge@mail.polimi.it>
2023-03-29 14:29:32 +0200

    Added groupid(to 0) method.

--- src/main/java/it/polimi/ingsw/model/Bookshelf.java
+++ src/main/java/it/polimi/ingsw/model/Bookshelf.java
@@ -254,5 +254,11 @@ public class Bookshelf {
             System.out.println();
         }
     }
-
+    public void setGroupIdCommon(){
+        for(int i=0; i< Rows; i++){
+            for(int j=0; j<Columns; j++){
+                shelfie[i][j].setGroupId(0);
+            }
+        }
+    }
 }
