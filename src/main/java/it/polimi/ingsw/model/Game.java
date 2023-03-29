3fcd79ae2a5a374ee7905207f107ba6efdf6847b
Amuro <federico.zanca@mail.polimi.it>
2023-03-29 14:31:18 +0200

    Re-added game instance

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import it.polimi.ingsw.model.commongoals.*;
 public class Game {
     //NOTE : creo il campo instance rendendo Game un singleton perché devo poi permettere al game di avere un solo controller che lo comandi (o è un bordello)
-    //private static Game instance;
+    private static Game instance;
 
     private static final int MAX_PLAYERS = 4;
     private int chosenNumOfPlayers;
@@ -34,22 +34,22 @@ public class Game {
      *
      * @return the singleton instance of the game
      */
-    /*
+
     public static Game getInstance() {
         if(instance == null)
             instance = new Game();
         return instance;
     }
-    */
+
 
     /**
      * Resets the game instance, all game data is lost after this operation.
      */
-    /*
+
     public static void resetInstance(){
         Game.instance=null;
     }
-    */
+
 
     /**
      * Sets the Board for the game
