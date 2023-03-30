4488895dae4c584736e002e9f3cc89c59aeb5f65
Amuro <federico.zanca@mail.polimi.it>
2023-03-30 23:11:33 +0200

    Added MessagType value to enum

--- src/main/java/it/polimi/ingsw/network/message/MessageType.java
+++ src/main/java/it/polimi/ingsw/network/message/MessageType.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.network.message;
 public enum MessageType {
     ERROR,
     DRAW_TILES,
-    INSERT_TILES
+    INSERT_TILES,
+
+    BOARD //il messaggio contiene la board aggiornata da dare alla view
 
 }
