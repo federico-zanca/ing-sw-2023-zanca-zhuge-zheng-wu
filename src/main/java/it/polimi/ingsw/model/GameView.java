522229408d9e4044dfc8578229f7d526c1fb6108
Amuro <federico.zanca@mail.polimi.it>
2023-04-04 14:10:01 +0200

    Added View interface methods

--- src/main/java/it/polimi/ingsw/model/GameView.java
+++ src/main/java/it/polimi/ingsw/model/GameView.java
@@ -1,5 +1,46 @@
 package it.polimi.ingsw.model;
 
-public class GameView {
+import it.polimi.ingsw.network.message.Message;
+import it.polimi.ingsw.utils.Observable;
+import it.polimi.ingsw.utils.Observer;
+import it.polimi.ingsw.view.View;
+
+public class GameView implements View, Observer {
+
+    @Override
+    public void update(Message message, Observable o) {
+
+    }
+
+    @Override
+    public void askUsername() {
+
+    }
+
+    @Override
+    public void askPlayerNumber() {
+
+    }
+
+    @Override
+    public void showLoginResult(boolean valiUsername, String username) {
+
+    }
+
+    @Override
+    public void showLobby() {
+
+    }
+
+    @Override
+    public void showFirstPlayerThisGame() {
+
+    }
+
+    @Override
+    public void showWinMessage() {
+
+    }
+
 
 }
