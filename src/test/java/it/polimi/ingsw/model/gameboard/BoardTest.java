d023b05dc4b67086f64cadbd4760cf819b4786ff
ZGMIKE2000 <zhenghaomichele.zhuge@mail.polimi.it>
2023-04-11 12:55:10 +0200

    Updated BoardTest.

--- src/test/java/it/polimi/ingsw/model/gameboard/BoardTest.java
+++ src/test/java/it/polimi/ingsw/model/gameboard/BoardTest.java
@@ -3,195 +3,163 @@ package it.polimi.ingsw.model.gameboard;
 import it.polimi.ingsw.model.Bag;
 import it.polimi.ingsw.model.ItemTile;
 import it.polimi.ingsw.model.enumerations.ItemType;
+import org.junit.Before;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 class BoardTest {
+    private Bag bag;
 
-    @Disabled
-    void testInitBoard() {
-        Board b = new Board();
-        /*tested also case 3, case 4, and case others.*/
-        b.initBoard(2);
-        b.printBoard();
+    private Board board;
+
+    public Board getBoard() {
+        return board;
+    }
+
+    @Before
+    public void setUp() {
+        board = null;
+        bag = null;
     }
 
     @Test
-    void testBoardMethods() {
-        Board b = new Board();
-        Bag bag = new Bag();
-        //initialize the board for 4 p
-        b.initBoard(4);
-        //Board needs refill because initBoard instantiates an empty board.
-        assertTrue(b.needsRefill());
-       // b.refillBoard(bag.drawItems(b.numCellsToRefill()));
-        //After refilling the board, it has to be full and needsRefill must be false.
-        assertEquals(0,b.numCellsToRefill());
-        assertFalse(b.needsRefill());
-        //Picking all items.
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                b.pickItem(i,j);
+        //3p and 4p can be repeated here.
+    void init2pBoardHasCorrectNumberOfEmptyCells() {
+        int counter = 0;
+        board = new Board();
+        board.initBoard(2);
+        for (int i = 0; i < Board.DIMENSIONS; i++) {
+            for (int j = 0; j < Board.DIMENSIONS; j++) {
+                if (board.getGameboard()[i][j].getItem().getType() == ItemType.EMPTY) {
+                    counter++;
+                } else if (board.getGameboard()[i][j].getItem().getType() != ItemType.FORBIDDEN) {
+                    fail();
+                }
             }
         }
-        //Placing two items that are separated.
-        b.placeItem(new ItemTile(ItemType.PLANT),0,3);
-        b.placeItem(new ItemTile(ItemType.PLANT),4,5);
-        //The board needs refill for the only two items are not adjacent.
-        assertEquals(45-2,b.numCellsToRefill());
-        assertTrue(b.needsRefill());
-        //Refills.
-       // b.refillBoard(bag.drawItems(b.numCellsToRefill()));
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                b.pickItem(i,j);
+        //3p should have expected 8 more, 4p 16 more.
+        assertEquals(29, counter);
+    }
+
+    @Test
+    void refillBoardWhenEmpty() {
+        board = new Board();
+        bag = new Bag();
+        board.initBoard(2);
+        ArrayList<ItemTile> items = bag.drawItems(29);
+        board.refillBoardWithItems(items);
+        int n = board.numCellsToRefill();
+        assertEquals(n, 0);
+    }
+    @Test
+    void refillBoardWhenNotEmpty() {
+        board = new Board();
+        bag = new Bag();
+
+        board.initBoard(2);
+        ArrayList<ItemTile> items = bag.drawItems(29);
+        board.refillBoardWithItems(items);
+
+        Board board1 = new Board();
+        board1.initBoard(2);
+        board1 = getBoard();
+        items = bag.drawItems(29);
+        board.refillBoardWithItems(items);
+        for (int i = 0; i < Board.DIMENSIONS; i++) {
+            for (int j = 0; j < Board.DIMENSIONS; j++) {
+                assertEquals(board.getGameboard()[i][j].getItem().getType(), board1.getGameboard()[i][j].getItem().getType());
             }
         }
-        //This time we place two items that are adjacent, so the board doesn't need refill.
-        b.placeItem(new ItemTile(ItemType.PLANT),4,4);
-        b.placeItem(new ItemTile(ItemType.PLANT),4,5);
-        //Method refillBoard shouldn't refill.
-       // b.refillBoard(bag.drawItems(b.numCellsToRefill()));
-        assertFalse(b.needsRefill());
+    }
+    @Test
+    void numCellsToRefill() {
+        board = new Board();
+        board.initBoard(2);
+        assertEquals(29,board.numCellsToRefill());
     }
 
     @Test
-    void testPlaceItem() {
-        Board b = new Board();
-        b.initBoard(3);
-        //Placing items on a forbidden square.
-        ItemTile item = new ItemTile(ItemType.BOOK);
-        b.placeItem(item,1,1);
-        assertTrue(b.getGameboard()[1][1].getItem().isForbidden());
-        //Placing items on a valid square, the item should be there.
-        item = new ItemTile(ItemType.BOOK);
-        b.placeItem(item,1,3);
-        assertEquals(ItemType.BOOK, b.getGameboard()[1][3].getItem().getType());
-        //Repeats above.
-        ItemTile item1 = new ItemTile(ItemType.CAT);
-        b.placeItem(item1,3,3);
-        assertEquals(ItemType.CAT, b.getGameboard()[3][3].getItem().getType());
+    void emptyBoardNeedsRefill() {
+        board = new Board();
+        board.initBoard(2);
+        assertTrue(board.needsRefill());
+    }
+    @Test
+    void singleItemsWithNoAdjacentsBoardNeedsRefill() {
+        board = new Board();
+        board.initBoard(2);
+        ItemTile item = new ItemTile(ItemType.PLANT);
+        board.placeItem(item,1,3);
+        board.placeItem(item,3,3);
+        board.placeItem(item,2,4);
+        assertTrue(board.needsRefill());
+    }
+    @Test
+    void filledBoardNeedsRefill() {
+        board = new Board();
+        bag = new Bag();
+        board.initBoard(2);
+        board.refillBoardWithItems(bag.drawItems(29));
+        assertFalse(board.needsRefill());
     }
 
     @Test
-    void testPickItem() {
-        Board b = new Board();
-        b.initBoard(2);
+    void placeItemWhenCellIsEmpty(){
+        board = new Board();
+        board.initBoard(2);
+        ItemTile item = new ItemTile(ItemType.PLANT);
+        board.placeItem(item, 1, 3);
+    }
+
+    @Test
+    void pickItem() {
+        board = new Board();
+        board.initBoard(2);
         //Placing items on a square and on adjacent squares.
         ItemTile item = new ItemTile(ItemType.BOOK);
-        b.placeItem(item,1,3);
+        board.placeItem(item,1,3);
         item = new ItemTile(ItemType.FRAME);
-        b.placeItem(item,1,4);
+        board.placeItem(item,1,4);
         item = new ItemTile(ItemType.CAT);
-        b.placeItem(item,2,3);
+        board.placeItem(item,2,3);
         //Picking one item, the square should be empty after picking and did not pick other item.
-        item = b.pickItem(1,3);
-        assertTrue(b.getGameboard()[1][3].getItem().isEmpty());
+        item = board.pickItem(1,3);
+        assertTrue(board.getGameboard()[1][3].getItem().isEmpty());
         assertEquals(ItemType.BOOK,item.getType());
+        assertNotNull(item);
     }
 
     @Disabled
-    void testEnableSquaresWithFreeSide() {
-        Board b = new Board();
-        Bag bag = new Bag();
-        b.initBoard(4);
-        //b.refillBoard(bag.drawItems(b.numCellsToRefill()));
-        b.enableSquaresWithFreeSide();
+    void pickItems() {
     }
-
-    @Disabled
-    void testPickableFirstItems() {
-        Board b = new Board();
-        b.initBoard(4);
-
-        ItemTile item = new ItemTile(ItemType.BOOK);
-        b.placeItem(item,2,3);
-        item = new ItemTile(ItemType.BOOK);
-        b.placeItem(item,2,4);
-        ItemTile item1 = new ItemTile(ItemType.CAT);
-        b.placeItem(item1,2,5);
-        ArrayList<Integer> pickables = b.pickableFirstItems();
-        int[] exp = new int[] {2, 3, 2, 4, 2, 5};
-        for (int i = 0; i < exp.length; i++){
-            assertEquals(exp[i], pickables.get(i));
-        }
-
-        ItemTile item2 = new ItemTile(ItemType.FRAME);
-        b.placeItem(item2,1,4);
-        item2 = new ItemTile(ItemType.FRAME);
-        b.placeItem(item2,3,4);
-        pickables = b.pickableFirstItems();
-        exp = new int[] {1, 4, 2, 3, 2, 5, 3, 4};
-        for (int i = 0; i < exp.length; i++){
-            assertEquals(exp[i], pickables.get(i));
-        }
-
-        item1 = new ItemTile(ItemType.CAT);
-        b.placeItem(item1,1,5);
-        item1 = new ItemTile(ItemType.CAT);
-        b.placeItem(item1,3,5);
-        pickables = b.pickableFirstItems();
-        exp = new int[] {1, 4, 1, 5, 2, 3, 2, 5, 3, 4, 3, 5};
-        for (int i = 0; i < exp.length; i++){
-            assertEquals(exp[i], pickables.get(i));
-        }
-    }
-
-    @Disabled
-    void testPickableItems() {
-        Board b = new Board();
-        b.initBoard(4);
-
-        ItemTile item = new ItemTile(ItemType.BOOK);
-        b.placeItem(item,2,3);
-        item = new ItemTile(ItemType.BOOK);
-        b.placeItem(item,2,4);
-        ItemTile item1 = new ItemTile(ItemType.CAT);
-        b.placeItem(item1,2,5);
-        ArrayList<Integer> pickables = b.pickableItems(2,3);
-        int[] exp = new int[] {2, 4, 2, 5};
-        for (int i = 0; i < exp.length; i++){
-            assertEquals(exp[i], pickables.get(i));
-        }
-
-        ItemTile item2 = new ItemTile(ItemType.FRAME);
-        b.placeItem(item2,1,4);
-        item2 = new ItemTile(ItemType.FRAME);
-        b.placeItem(item2,3,4);
-        pickables = b.pickableItems(1,4);
-        exp = new int[] {2, 4, 3, 4};
-        for (int i = 0; i < exp.length; i++){
-            assertEquals(exp[i], pickables.get(i));
-        }
-
-        item1 = new ItemTile(ItemType.CAT);
-        b.placeItem(item1,1,5);
-        item1 = new ItemTile(ItemType.CAT);
-        b.placeItem(item1,3,5);
-        pickables = b.pickableItems(1,5);
-        exp = new int[] {2, 5, 3, 5, 1, 4};
-        for (int i = 0; i < exp.length; i++){
-            assertEquals(exp[i], pickables.get(i));
-        }
+    @Test
+    void testDoesSquareHaveFreeSide() {
+        board = new Board();
+        bag = new Bag();
+        board.initBoard(4);
+        board.refillBoardWithItems(bag.drawItems(board.numCellsToRefill()));
+        board.pickItem(3,3);
+        //After picking one item, the squares adjacent should have a free side.
+        assertTrue(board.doesSquareHaveFreeSide(3,2));
+        assertTrue(board.doesSquareHaveFreeSide(3,4));
+        assertTrue(board.doesSquareHaveFreeSide(2,3));
+        assertTrue(board.doesSquareHaveFreeSide(4,3));
+        //the squares on the board edge all have free sides.(manually repeated test on all squares)
+        assertTrue(board.doesSquareHaveFreeSide(1,3));
     }
 
     @Test
-    void testDoesSquareHaveFreeSide() {
-        Board b = new Board();
-        Bag bag = new Bag();
-        b.initBoard(4);
-       // b.refillBoard(bag.drawItems(b.numCellsToRefill()));
-        b.pickItem(3,3);
-        //After picking one item, the squares adjacent should have a free side.
-        assertTrue(b.doesSquareHaveFreeSide(3,2));
-        assertTrue(b.doesSquareHaveFreeSide(3,4));
-        assertTrue(b.doesSquareHaveFreeSide(2,3));
-        assertTrue(b.doesSquareHaveFreeSide(4,3));
-        //the squares on the board edge all have free sides.(manually repeated test on all squares)
-        assertTrue(b.doesSquareHaveFreeSide(1,3));
+    void disableAllSquares() {
+    board = new Board();
+    board.initBoard(2);
+    board.enableSquaresWithFreeSide();
+    board.disableAllSquares();
+        for(int i=0; i<Board.DIMENSIONS; i++){
+            for(int j=0; j<Board.DIMENSIONS; j++){
+                assertFalse(board.getGameboard()[i][j].isPickable());
+            }
+        }
     }
 }
