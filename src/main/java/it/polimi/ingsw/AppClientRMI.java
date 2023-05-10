0532b634281350207e1443c1a6174079ab7b4f24
Amuro <federico.zanca@mail.polimi.it>
2023-05-11 00:57:24 +0200

    Added option to choose between TUI and GUI (dummy, real implementation is commented)

--- src/main/java/it/polimi/ingsw/AppClientRMI.java
+++ src/main/java/it/polimi/ingsw/AppClientRMI.java
@@ -2,28 +2,50 @@ package it.polimi.ingsw;
 
 import it.polimi.ingsw.distributed.Server;
 import it.polimi.ingsw.distributed.ClientImpl;
+import it.polimi.ingsw.view.TextualUI;
+import it.polimi.ingsw.view.gui.Gui;
+import it.polimi.ingsw.view.gui.JavaFXApp;
+import javafx.application.Application;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 public class AppClientRMI {
 
     public static void main(String[] args) throws RemoteException, NotBoundException {
-        System.out.println(
-                "███╗   ███╗██╗   ██╗███████╗██╗  ██╗███████╗██╗     ███████╗██╗███████╗\n" +
-                "████╗ ████║╚██╗ ██╔╝██╔════╝██║  ██║██╔════╝██║     ██╔════╝██║██╔════╝\n" +
-                "██╔████╔██║ ╚████╔╝ ███████╗███████║█████╗  ██║     █████╗  ██║█████╗  \n" +
-                "██║╚██╔╝██║  ╚██╔╝  ╚════██║██╔══██║██╔══╝  ██║     ██╔══╝  ██║██╔══╝  \n" +
-                "██║ ╚═╝ ██║   ██║   ███████║██║  ██║███████╗███████╗██║     ██║███████╗\n" +
-                "╚═╝     ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝\n" +
-                "                                                                       \n" +
-                "\n");
         Registry registry = LocateRegistry.getRegistry(1099);
         AppServer server = (AppServer) registry.lookup("server");
 
         ClientImpl client = new ClientImpl(server.connect());
-        client.run();
+        System.out.println("Inserisci 0 per avviare la TUI, qualsiasi altro carattere o parola per la GUI");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        if (s.equals("0")) {
+            client.run();
+        } else {
+            Application.launch(JavaFXApp.class);
+        }
+
+        /*
+            if (args.length > 0 && args[0].equalsIgnoreCase("CLI")) {
+                System.out.println(
+                        "███╗   ███╗██╗   ██╗███████╗██╗  ██╗███████╗██╗     ███████╗██╗███████╗\n" +
+                                "████╗ ████║╚██╗ ██╔╝██╔════╝██║  ██║██╔════╝██║     ██╔════╝██║██╔════╝\n" +
+                                "██╔████╔██║ ╚████╔╝ ███████╗███████║█████╗  ██║     █████╗  ██║█████╗  \n" +
+                                "██║╚██╔╝██║  ╚██╔╝  ╚════██║██╔══██║██╔══╝  ██║     ██╔══╝  ██║██╔══╝  \n" +
+                                "██║ ╚═╝ ██║   ██║   ███████║██║  ██║███████╗███████╗██║     ██║███████╗\n" +
+                                "╚═╝     ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝\n" +
+                                "                                                                       \n" +
+                                "\n");
+                new TextualUI().run();
+            } else {
+                Application.launch(JavaFXApp.class);
+            }
+    }
+
+         */
     }
 }
