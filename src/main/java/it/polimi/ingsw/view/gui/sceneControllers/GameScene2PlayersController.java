b0b581ab3f9f735d96e0c234efac304c9e50fe6e
Jhui0908 <jiahui.wu@mail.polimi.it>
2023-05-18 00:32:33 +0200

    first template of game scene 2 players

---
+++ src/main/java/it/polimi/ingsw/view/gui/sceneControllers/GameScene2PlayersController.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.view.gui.sceneControllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class GameScene2PlayersController {
+
+    @FXML
+    private ImageView bag;
+
+    @FXML
+    private ImageView firstCommonGoal;
+
+    @FXML
+    private ImageView livingRoomBoard;
+
+    @FXML
+    private ImageView myBookshelf;
+
+    @FXML
+    private ImageView myPersonalGoal;
+
+    @FXML
+    private Label nameOrth1OnBookshelf;
+
+    @FXML
+    private Label namePlayerOnBookshelf;
+
+    @FXML
+    private ImageView oth1BookShelf;
+
+    @FXML
+    private VBox players;
+
+    @FXML
+    private ImageView secondCommonGoal;
+
+}
