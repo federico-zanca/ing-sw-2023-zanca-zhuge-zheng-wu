4a78a3b51278a0c4887d87b74dbbbd14cb2513d4
Jhui0908 <jiahui.wu@mail.polimi.it>
2023-05-19 22:03:20 +0200

    fixed in lobby scene

--- src/main/java/it/polimi/ingsw/view/gui/sceneControllers/InLobbySceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/sceneControllers/InLobbySceneController.java
@@ -73,7 +73,7 @@ public class InLobbySceneController implements Controller{
         Platform.runLater(()->{
             playerNames.getChildren().clear();
             Label label = new Label(username);
-            label.setStyle("-fx-font-weight: regular;-fx-font-style: italic; -fx-text-fill: #000000; -fx-font-size: 18; -fx-text-alignment: left; -fx-pref-width: 600;");
+            label.setStyle("-fx-font-weight: regular;-fx-font-style: italic; -fx-text-fill: #000000; -fx-font-size: 18; -fx-text-alignment: left; -fx-pref-width: 600;-fx-pref-height: 40;");
             playerNames.getChildren().add(label);
         });
     }
