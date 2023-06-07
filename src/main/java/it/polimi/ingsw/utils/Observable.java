fae1e00192eb7606856a111983be38e4aa86d614
Jhui0908 <jiahui.wu@mail.polimi.it>
2023-06-07 21:26:12 +0200

    javadoc : utils (observer and observable)

--- src/main/java/it/polimi/ingsw/utils/Observable.java
+++ src/main/java/it/polimi/ingsw/utils/Observable.java
@@ -5,13 +5,15 @@ import it.polimi.ingsw.network.message.Message;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Observable class that can be observed by implementing the {@link Observer} interface and registering as listener.
+ */
 public class Observable {
     private List<Observer> observers = new ArrayList<>();
 
 
     /**
      * Adds an observer.
-     *
      * @param o the observer to be added.
      */
     public synchronized void addObserver(Observer o) {
@@ -23,7 +25,6 @@ public class Observable {
 
     /**
      * Removes an observer.
-     *
      * @param obs the observer to be removed.
      */
     public synchronized void removeObserver(Observer obs) {
@@ -32,7 +33,6 @@ public class Observable {
 
     /**
      * Notifies all the current observers through the update method and passes to them a {@link Message}.
-     *
      * @param message the message to be passed to the observers.
      */
     public void notifyObservers(Message message) {
@@ -41,6 +41,10 @@ public class Observable {
         }
     }
 
+    /**
+     * Removes the observer at the specified index from the list of observers.
+     * @param index the index of the observer to be removed.
+     */
     public void removeObserver(int index) {
         observers.remove(index);
     }
--- src/main/java/it/polimi/ingsw/utils/Observer.java
+++ src/main/java/it/polimi/ingsw/utils/Observer.java
@@ -2,6 +2,9 @@ package it.polimi.ingsw.utils;
 
 import it.polimi.ingsw.network.message.Message;
 
+/**
+ * Observer interface. It supports a generic method of update.
+ */
 public interface Observer {
     void update(Message message);
 }
